798ac22ed74b291fafd1816af81ad83020d4b395
antho1404 <anthony.estebe@gmail.com>
2025-04-16 00:18:17 +0700

    ignore ssh2

--- next.config.ts
+++ next.config.ts
@@ -10,13 +10,15 @@ const nextConfig: NextConfig = {
       { hostname: "avatars.githubusercontent.com", pathname: "/u/*" },
     ],
   },
-  // webpack: (config) => {
-  //   config.module.rules.push({
-  //     test: /sshcrypto\.node$/,
-  //     use: "ignore-loader",
-  //   });
-  //   return config;
-  // },
+  webpack: (config) => {
+    config.externals.push("ssh2");
+    // ssh2;
+    // config.module.rules.push({
+    //   test: /sshcrypto\.node$/,
+    //   use: "ignore-loader",
+    // });
+    return config;
+  },
 };
 
 export default nextConfig;
